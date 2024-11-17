d6fcbba464e3cacd6156f7c3d8aa05c4faaaeafb
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-11-17 13:21:56 -0800

    Inital Commit

--- dog.py
+++ dog.py
@@ -1,9 +1,15 @@
 # dog.py
 class Dog:
-  def __init__(self, name, breed):
-    self.name = name
-    self.breed = breed
-    print("dog initialized!")
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
 
-  def bark(self):
-    print("Woof!")
+    def bark(self):
+        print("Woof!")
+
+    def eat(self):
+        print(f"{self.name} is eating!")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping!")
--- my_dogs.py
+++ my_dogs.py
@@ -1,9 +1,12 @@
 # my_dogs.py
-import dog 
+from dog import Dog
 
-my_dog = dog.Dog("Rex", "SuperDog")
-my_other_dog = dog.Dog("Annie", "SuperDog")
+my_dog = Dog("Rex", "SuperDog")
+my_other_dog = Dog("Annie", "SuperDog")
 
-my_dog.bark()
+my_dog.bark() 
+my_dog.eat() 
+my_dog.sleep()
 print(my_dog.name)
 print(my_other_dog.name)
+
