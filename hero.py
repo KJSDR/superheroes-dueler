0296691efa1717c38d8335eae131fe4d0b8cb38a
KJSDR <killian.delamarre-ruer@students.dominican.edu>
2024-11-20 10:55:23 -0800

    Added more to heroes

--- hero.py
+++ hero.py
@@ -1,34 +1,93 @@
 # hero.py
-import random
+from ability import Ability
+from armor import Armor
 
 class Hero:
     def __init__(self, name, starting_health=100):
         '''Instance properties:
+            abilities: List
+            armors: List
             name: String
             starting_health: Integer
             current_health: Integer
         '''
+        self.abilities = list()
+        self.armors = list()
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
 
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+        self.abilities.append(ability)
+
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+            return: total_damage:Int
+        '''
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def add_armor(self, armor):
+        '''Add armor to self.armors'''
+        self.armors.append(armor)
+
+    def defend(self):
+        '''Calculate the total block amount from all armor blocks.
+           return: total_block:Int
+        '''
+        total_block = 0
+        for armor in self.armors:
+            total_block += armor.block()
+        return total_block
+
+    def take_damage(self, damage):
+        '''Updates self.current_health to reflect the damage minus the defense.'''
+        blocked_damage = self.defend()
+        actual_damage = max(0, damage - blocked_damage) 
+        self.current_health -= actual_damage
+
+    def is_alive(self):
+        '''Return True or False depending on whether the hero is alive or not.'''
+        return self.current_health > 0
+
     def fight(self, opponent):
         '''Current Hero will take turns fighting the opponent hero passed in.'''
-        
-        total_health = self.starting_health + opponent.starting_health
-        self_chance = self.starting_health / total_health
-        opponent_chance = opponent.starting_health / total_health
+        while True:
+            if not self.abilities and not opponent.abilities:
+                print("Draw")
+                return
 
-        
-        winner = random.choices([self, opponent], [self_chance, opponent_chance])[0]
+            self_damage = self.attack()
+            opponent_damage = opponent.attack()
 
-        
-        print(f"{winner.name} defeats {self.name if winner != self else opponent.name}!")
+            opponent.take_damage(self_damage)
+
+            if not opponent.is_alive():
+                print(f"{self.name} won!")
+                return
+
+            self.take_damage(opponent_damage)
+
+            if not self.is_alive():
+                print(f"{opponent.name} won!")
+                return
 
 
 if __name__ == "__main__":
-    
-    hero1 = Hero("Wonder Woman", 300)
-    hero2 = Hero("Dumbledore", 250)
+    # If you run this file from the terminal
+    # this block is executed.
 
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
     hero1.fight(hero2)
